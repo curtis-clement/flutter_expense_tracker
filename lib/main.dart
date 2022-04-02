b9dc6efa7a97ca9197168e0fd1617b6a8d0879b9
Curtis Clement <curtis.clement@wonderkind.com>
2022-04-02 16:44:23 +0200

    add date formatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import './transaction.dart';
 
 void main() => runApp(MyApp());
@@ -84,7 +85,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          item.date.toString(),
+                          DateFormat.yMMMd().format(item.date),
                           style: TextStyle(
                             color: Colors.grey,
                           ),
